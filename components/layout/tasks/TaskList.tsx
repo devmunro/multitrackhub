4c10cce3b2ba32d43351dc3afaccec9440746e28
devmunro <defmunro@gmail.com>
2024-02-06 16:51:32 +0000

    Add ColumnDef import to TaskList component

--- components/layout/tasks/TaskList.tsx
+++ components/layout/tasks/TaskList.tsx
@@ -3,13 +3,14 @@ import { columns } from "./Columns";
 import { DataTable } from "../../ui/DataTable";
 import { useState, useEffect } from "react";
 import taskData from "../../../data/taskDummyData.json";
+import { ColumnDef } from "@tanstack/react-table";
 
 interface Task {
-    title: string;
-    status: string;
-    group: string;
-    rhythm: string;
-  }
+  title: string;
+  status: string;
+  group: string;
+  rhythm: string;
+}
 
 export default function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -39,7 +40,11 @@ export default function TaskList() {
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={tasks} onAddTask={handleAddTask} />
+      <DataTable
+        columns={columns as any}
+        data={tasks}
+        onAddTask={handleAddTask as any}
+      />
     </div>
   );
 }
