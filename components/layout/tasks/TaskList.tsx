0f512b76da6fb8e84233c443e3af2aea2f36abe8
devmunro <defmunro@gmail.com>
2024-02-17 12:05:11 +0000

    Remove unused import

--- components/layout/tasks/TaskList.tsx
+++ components/layout/tasks/TaskList.tsx
@@ -3,7 +3,6 @@ import { columns } from "./Columns";
 import { DataTable } from "./DataTable";
 import { useEffect } from "react";
 import { useTaskStore } from "@/lib/store";
-import { Task } from "@/lib/store";
 
 export default function TaskList() {
   const { tasks, fetchTasks } = useTaskStore();
